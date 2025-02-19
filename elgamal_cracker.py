623a5113ea98e33fe13459069a6de82516d1c28e
rohan37-pro <rohan2002maji@gmail.com>
2025-02-19 09:30:37 +0530

    elgamal is cracked wooh hooo

--- elgamal_cracker.py
+++ elgamal_cracker.py
@@ -1,13 +1,38 @@
 import string
+import time
+
+slow = 0.05
 
 cipher = list(eval(input("Enter Cipher : ")))
+print()
 
 message = []
 
 beta = None
-while len(message)<len(cipher):
-    
-    for c in cipher:
+start = 0
+start_again = True
+while start<len(string.printable) :
+    if start_again:
+        beta = int(cipher[0] / ord(string.printable[start]))
+        start_again = False
+        message.append(string.printable[start])
+        print("".join(message), end='\r')
+        time.sleep(slow)
+    for  c in cipher[1:]:
         for m in string.printable:
-            if beta == None:
-                beta 
+            if int(c/ord(m)) == beta:
+                time.sleep(slow)
+                message.append(m)
+                print("".join(message), end='\r')
+                found = True
+                break
+        else:
+            start+=1
+            start_again = True
+            message = []
+            break
+    if len(message)==len(cipher):
+        print(f"\"{''.join(message)}\"")
+        start+=1
+        start_again = True
+        message = []
